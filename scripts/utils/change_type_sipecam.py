9a969a8b957fb9bec8c4c1c406dcd9660ede8945
jdonlucas <jdonlucas93@gmail.com>
2022-04-12 14:18:56 -0500

    change func for adding types/aspects to file

--- scripts/utils/change_type_sipecam.py
+++ scripts/utils/change_type_sipecam.py
@@ -1,10 +1,12 @@
 import os
-import json
 import re
-import itertools
 import glob
 import time
+import json
+import itertools
 import datetime as dt
+from os.path import exists as file_exists
+
 from utils import login
 from helpers import BASE_ENDPOINT
 from helpers.globals import AUDIO_PATTERNS, VIDEO_PATTERNS, IMAGE_PATTERNS
@@ -42,10 +44,35 @@ def change_type_sipecam(session, root_folder_id, path_to_files, recursive):
 
     starttime = time.time()
 
+
+    dirs_with_data = []
+    for file_with_path in files_in_dir:
+
+        # taking length of the path
+        len_of_path = len(file_with_path.split("/"))
+        
+        # obtaining the name of the file
+        name_of_file = file_with_path.split("/")[len_of_path - 1]
+
+        # extracting the full path to the file
+        root_dir_path = file_with_path.replace(name_of_file, "")
+
+        if root_dir_path not in dirs_with_data and "HLPX" in root_dir_path:
+            dirs_with_data.append(root_dir_path)
+
+    filename = "logs/type_n_aspects_log" + path_to_files.replace('/','-') + '.txt'
+    os.makedirs(os.path.dirname(filename), exist_ok=True)
+    if file_exists(filename):
+        with open(filename, 'r') as f:
+            lines_in_file = [line.replace('\n','') for line in f]
+        
+        tmp_list = [x for x in dirs_with_data if x not in lines_in_file]
+
+        dirs_with_data = tmp_list
+
     try:
         updated = []
-        for idx, file_with_path in enumerate(files_in_dir):
-
+        for d in dirs_with_data:
             
             # total time since last login or script start
             total_time = round((time.time() - starttime), 2)
@@ -66,111 +93,182 @@ def change_type_sipecam(session, root_folder_id, path_to_files, recursive):
                 time.sleep(5)
                 print("Login sucessful, continuing upload\n")
 
-            len_of_path = len(file_with_path.split("/"))
-            name_of_file = file_with_path.split("/")[len_of_path - 1]
-            if re.match('[a-zA-Z_]*[0-9]*-[0-9]*-[0-9]*', name_of_file): 
-                root_dir_path = file_with_path.replace(path_to_files, "").replace(
-                    file_with_path.split("/")[len_of_path - 1], ""
-                )
+            # filter jsons for current path
+            get_json_files_for_dir = [j for j in files_in_dir if d in j]
 
-                response = session.get(
-                    os.getenv("ALFRESCO_URL")
-                    + BASE_ENDPOINT
-                    + "/nodes/"
-                    + root_folder_id
-                    + "/children?relativePath="+root_dir_path+"&include=aspectNames&skipCount=0&maxItems=1"
-                )
+            # init latest date and json file url
+            latest_json_file = ''
+            latest_date = None
+            for json_file in get_json_files_for_dir:
 
-                if response.status_code == 200:
+                # split the path taking / as delimiter
+                len_of_path = len(json_file.split("/"))
+                
+                # obtaining the name of the file, last item in path
+                name_of_file = json_file.split("/")[len_of_path - 1]
 
-                    print("Changing type of files in " + root_dir_path + "\n\n")
+                # validate if json file matches standard name
+                if re.match('[a-zA-Z_]*[0-9]*-[0-9]*-[0-9]*', name_of_file): 
 
-                    has_more_items = True
-                    skip_count = 0
+                    date_of_file = name_of_file.split("_")[len(name_of_file.split("_")) - 1]
 
-                    while has_more_items:
+                    # parse it to a datetime object
+                    date = dt.datetime.strptime(date_of_file.replace(".json",''), '%d-%m-%Y')
 
-                        response = session.get(
-                            os.getenv("ALFRESCO_URL")
-                            + BASE_ENDPOINT
-                            + "/nodes/"
-                            + root_folder_id
-                            + "/children?relativePath="+root_dir_path+"&include=aspectNames&skipCount=" + str(skip_count) + "&maxItems=100"
-                        )
+                    # check if date is the latest date
+                    if latest_date and latest_date < date:
+                        latest_date = date
+                        latest_json_file = json_file
+                    elif not latest_date:
+                        latest_date = date
+                        latest_json_file = json_file
 
-                        has_more_items = response.json()["list"]["pagination"]["hasMoreItems"]
-                        data_file = open(file_with_path)
+            # get dir path, which is the same in alfresco
+            root_dir_path = latest_json_file.replace(path_to_files, "").replace(
+                latest_json_file.split("/")[len_of_path - 1], ""
+            )
 
-                        data_json = json.load(data_file)
+            # make a query to relative path to check if exists
+            response = session.get(
+                os.getenv("ALFRESCO_URL")
+                + BASE_ENDPOINT
+                + "/nodes/"
+                + root_folder_id
+                + "/children?relativePath="+root_dir_path+"&include=aspectNames&skipCount=0&maxItems=1"
+            )
 
-                        for f in response.json()["list"]["entries"]:
+            # error flag
+            is_error = False
 
-                            if f["entry"]["isFile"]:
+            # if request is successful then continue
+            if response.status_code == 200:
 
-                                found = None
-                                for i in data_json["MetadataFiles"].keys():
-                                    len_complete_path = len(i.split("/"))
-                                    filename = i.split("/")[len_complete_path - 1]
-                                    if filename == f["entry"]["name"]:
-                                        found = i
-                                        break
-                                
-                                prop_dict = {}
+                print("Changing type of files in " + root_dir_path + "\n\n")
 
-                                if any(filetype in f["entry"]["name"] for filetype in IMAGE_PATTERNS):
-                                    new_type = "sipecamImage:ImageSipecam"
-                                elif any(filetype in f["entry"]["name"] for filetype in VIDEO_PATTERNS):
-                                    new_type = "sipecamVideo:sipecamVideo"
-                                elif any(filetype in f["entry"]["name"] for filetype in AUDIO_PATTERNS):
-                                    new_type = "sipecamAudio:audiofileSipecam"
-            
-                                prop_dict["sipecam:NomenclatureNode"] = data_json["MetadataDevice"]["NomenclatureNode"]
-                                prop_dict["sipecam:CumulusName"] = data_json["MetadataDevice"]["CumulusName"]
-                                prop_dict["sipecam:EcosystemsName"] = data_json["MetadataDevice"]["EcosystemsName"]
-                                prop_dict["sipecam:NodeCategoryIntegrity"] = data_json["MetadataDevice"]["NodeCategoryIntegrity"]
+                has_more_items = True
+                skip_count = 0
 
-                                if found:
-                                    file_metadata = data_json["MetadataFiles"][found]
-                                    for key in file_metadata:
-                                        if "date" not in key.lower():
-                                            prop_dict[new_type.split(":")[0] + ":" + key] = file_metadata[key]
+                # change type, add aspects to file for all files in location
+                while has_more_items:
+
+                    response = session.get(
+                        os.getenv("ALFRESCO_URL")
+                        + BASE_ENDPOINT
+                        + "/nodes/"
+                        + root_folder_id
+                        + "/children?relativePath="+root_dir_path+"&include=aspectNames&skipCount=" + str(skip_count) + "&maxItems=100"
+                    )
+
+                    has_more_items = response.json()["list"]["pagination"]["hasMoreItems"]
+                    data_file = open(latest_json_file)
+
+                    data_json = json.load(data_file)
+
+                    # for each file in request change type/add aspect
+                    for f in response.json()["list"]["entries"]:
+
+                        if f["entry"]["isFile"]:
+
+                            # find match in json file with file in request
+                            found = None
+                            for i in data_json["MetadataFiles"].keys():
+                                len_complete_path = len(i.split("/"))
+                                filename = i.split("/")[len_complete_path - 1]
+                                if filename.replace("AVI","mp4") == f["entry"]["name"]:
+                                    found = i
+                                    break
+                                        
+                            prop_dict = {}
+
+                            # select specific type for file
+                            if any(filetype in f["entry"]["name"] for filetype in IMAGE_PATTERNS):
+                                new_type = "sipecam:image"
+                            elif any(filetype in f["entry"]["name"] for filetype in VIDEO_PATTERNS):
+                                new_type = "sipecam:video"
+                            elif any(filetype in f["entry"]["name"] for filetype in AUDIO_PATTERNS):
+                                new_type = "sipecam:audio"
+
+                            # fill data that is not included in file metadata, but corresponds to device metadata
+                            prop_dict["sipecam:CumulusName"] = data_json["MetadataDevice"]["CumulusName"]
+                            prop_dict["sipecam:EcosystemsName"] = data_json["MetadataDevice"]["EcosystemsName"]
+                            prop_dict["sipecam:NodeCategoryIntegrity"] = data_json["MetadataDevice"]["NodeCategoryIntegrity"]
+                            prop_dict["sipecam:NomenclatureNode"] = data_json["MetadataDevice"]["NomenclatureNode"]
+
+                            date_deployment_array = data_json["MetadataDevice"]["DateDeployment"].split("-")
+                            date_deployment = date_deployment_array[2] + "-" + date_deployment_array[1] + date_deployment_array[0]
+                            prop_dict["sipecam:DateDeployment"] = date_deployment
+
+                            if found:
+                                file_metadata = data_json["MetadataFiles"][found]
+                                for key in file_metadata:
+                                    if "date" not in key.lower():
+                                        if "GPS" in key:
+                                            prop_dict[new_type.split(":")[0] + ":" + key.replace("GPS",'')] = file_metadata[key]
                                         else:
-                                            try:
-                                                prop_dict[new_type.split(":")[0] + ":" + key] = (
-                                                    dt.datetime.strptime(file_metadata[key], "%H:%M:%S %d/%m/%Y (%Z%z)").strftime(
-                                                        "%Y-%m-%dT%H:%M:%S.%f%z")
-                                                )
-                                            except:
-                                                prop_dict[new_type.split(":")[0] + ":" + key] = (
-                                                    dt.datetime.strptime(file_metadata[key], "%Y:%m:%d %H:%M:%S").strftime(
-                                                        "%Y-%m-%dT%H:%M:%S.%f%z")
-                                                )
+                                            prop_dict[new_type.split(":")[0] + ":" + key] = file_metadata[key]
+                                    else:
+                                        if "datetime" in key.lower() and "original" not in key.lower():
+                                            key_name = new_type.split(":")[0] + ":" + key + "Original"
+                                        else:
+                                            key_name = new_type.split(":")[0] + ":" + key
+                                        try:
+                                            prop_dict[key_name] = (
+                                                dt.datetime.strptime(file_metadata[key], "%H:%M:%S %d/%m/%Y (%Z%z)").strftime(
+                                                    "%Y-%m-%dT%H:%M:%S.%f%z")
+                                            )
+                                        except:
+                                            prop_dict[key_name] = (
+                                                dt.datetime.strptime(file_metadata[key], "%Y:%m:%d %H:%M:%S").strftime(
+                                                    "%Y-%m-%dT%H:%M:%S.%f%z")
+                                            )
 
-                                else:
-                                    print("File " + f["entry"]["name"] + " not found in json file")
+                            else:
+                                print("File " + f["entry"]["name"] + " not found in json file")
 
-                                aspects = f["entry"]["aspectNames"]
+                            aspects = f["entry"]["aspectNames"]
 
-                                aspects.append("sipecam:devicemetadata")
+                            # add aspects for common properties
+                            aspects.append("sipecam:common")
+                            aspects.append("sipecam:fileDetails")
 
-                                data = {"aspectNames": aspects, "nodeType": new_type, "properties": prop_dict}
+                            # aspects for common properties in specific types
+                            if any(filetype in f["entry"]["name"] for filetype in IMAGE_PATTERNS):
+                                aspects.append("sipecam:imageVideoCommons")
+                            elif any(filetype in f["entry"]["name"] for filetype in VIDEO_PATTERNS):
+                                aspects.append("sipecam:imageVideoCommons")
+                                aspects.append("sipecam:audioVideoCommons")
+                            elif any(filetype in f["entry"]["name"] for filetype in AUDIO_PATTERNS):
+                                aspects.append("sipecam:audioVideoCommons")
 
-                                update = session.put(
-                                    os.getenv("ALFRESCO_URL")
-                                    + BASE_ENDPOINT
-                                    + "/nodes/"
-                                    + f["entry"]["id"],
-                                    data=json.dumps(data),
-                                )
-                                if "error" in update.json().keys():
-                                    print("\n\n")
-                                    print(update.json())
-                                    print("\n\n")
-                                updated.append(update.json())
+                            # collect update data
+                            data = {"aspectNames": aspects, "nodeType": new_type, "properties": prop_dict}
 
-                                skip_count = skip_count + 100
-                                
-                                print("Updated " + f["entry"]["name"])
+                            # update properties request
+                            update = session.put(
+                                os.getenv("ALFRESCO_URL")
+                                + BASE_ENDPOINT
+                                + "/nodes/"
+                                + f["entry"]["id"],
+                                data=json.dumps(data),
+                            )
+
+                            # if theres an error, output to console
+                            if "error" in update.json().keys():
+                                is_error = True
+                                print("\n\n")
+                                print(update.json())
+                                print("\n\n")
+                            updated.append(update.json())
+
+                            # skip to next batch
+                            skip_count = skip_count + 100
+
+                            print("Updated " + f["entry"]["name"])
+
+            if not is_error:
+                filename = "logs/type_n_aspects_log" + path_to_files.replace('/','-') + '.txt'
+                with open(filename, 'a') as log_file:
+                    log_file.writelines("%s\n" % d)
 
         return updated
 
